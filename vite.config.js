574a87579af8539ba2f3f020ba24f014abd41a0a
David Lazaro <dlazaro7@gmail.com>
2023-11-17 17:57:48 +0100

    vit+react cleaned with basic css

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+        {
+            // this is required for the SCSS modules
+            find: /^~(.*)$/,
+            replacement: '$1',
+        },
+    ],
+},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    hmr: { overlay: false },
+  },
+})
