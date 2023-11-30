5fb684eff62c27598c1015a3f6a963e872c5c0ea
David Lazaro <dlazaro7@gmail.com>
2023-11-30 01:33:03 +0100

    dev icons in projects added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,10 @@
 import "./portfolio.scss";
 
 import portfolioData from "../Portfolio/data.json";
+import { DiCss3, DiReact, DiHtml5, DiNodejsSmall } from "react-icons/di";
+import { SiMongodb, SiExpress, SiAxios, SiJsonwebtokens } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { BiLogoTypescript, BiLogoJavascript, BiLogoSass  } from "react-icons/bi";
 
 const Portfolio = () => {
   const renderPortfolio = (portfolio) => {
@@ -17,6 +21,26 @@ const Portfolio = () => {
               <div className="content">
                 <p className="title">{port.name}</p>
                 <h4 className="description">{port.description}</h4>
+                <h4 className="description">
+                  {port.tech.map((eachTech, idx) => {
+                    return (
+                        
+                    <p key={idx}>{eachTech === "js" ? <BiLogoJavascript /> : "" 
+                          || eachTech === "react" ? <DiReact />: "" 
+                          || eachTech === "html" ? <DiHtml5 /> : "" 
+                          || eachTech === "css" ? <DiCss3/>: ""
+                          || eachTech === "mongo" ? <SiMongodb />: ""
+                          || eachTech === "node" ? <DiNodejsSmall />: ""
+                          || eachTech === "express" ? <SiExpress />: ""
+                          || eachTech === "axios" ? <SiAxios />: ""
+                          || eachTech === "jwt" ? <SiJsonwebtokens />: ""
+                          || eachTech === "next" ? <TbBrandNextjs />: ""
+                          || eachTech === "ts" ? <BiLogoTypescript />: ""
+                          || eachTech === "sass" ? <BiLogoSass />: ""                  
+                         }</p>                                                           
+                    );
+                  })}
+                </h4>
                 <button
                   className="btn"
                   onClick={() => window.open(port.webUrl)}
